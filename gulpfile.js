e23b48ca26cde189a48d72d28cf85e32e5d2a2d3
PhoenixDev - OK <oleksandr.kovinka@phoenix-media.eu>
2018-09-15 20:00:36 +0300

    updated npm modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var sass = require('gulp-sass')
 
 // Static Server + watching scss/js/html files
 gulp.task('default', function() {
-    browserSync.init({
-        server: "./"
-    });
+  browserSync.init({
+    server: './'
+  })
 
-    gulp.watch("./scss/*.scss", ['sass']);
-    gulp.watch("./js/*js").on('change', browserSync.reload);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  gulp.watch('./scss/**/*.scss', ['sass'])
+  gulp.watch('./js/*js').on('change', browserSync.reload)
+  gulp.watch('./*.html').on('change', browserSync.reload)
+})
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("./scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
-});
+  return gulp
+    .src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream())
+})
